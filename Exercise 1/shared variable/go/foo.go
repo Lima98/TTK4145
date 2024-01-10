66bb1cdd9449c7eff2bbdf13c8fa22f9de7d568d
martineengen <testing@gmail.com>
2024-01-10 10:55:31 +0100

    lagt til exercice 1.3.go

--- Exercise 1/shared variable/go/foo.go
+++ Exercise 1/shared variable/go/foo.go
@@ -3,26 +3,39 @@
 package main
 
 import (
-    . "fmt"
-    "runtime"
-    "time"
+	. "fmt"
+	"runtime"
+	"time"
 )
 
 var i = 0
 
 func incrementing() {
     //TODO: increment i 1000000 times
+    for j := 0; j < 1000000; j++ {
+        i++;
+	}
+
 }
 
 func decrementing() {
     //TODO: decrement i 1000000 times
+    for j := 0; j < 1000000; j++ {
+        i--;
+    }
 }
 
 func main() {
     // What does GOMAXPROCS do? What happens if you set it to 1?
+    //GOMAXPROCS setter maks antall prosesser (her tråder) som kan kjøres samtidig. Sette til 1--> kan bare kjøre en tråd 
     runtime.GOMAXPROCS(2)    
 	
     // TODO: Spawn both functions as goroutines
+    //Goroutine er basically en thread i Go, altså det som kaller på funksjonen, og får funksjonen til å kjøre
+
+    go incrementing();
+    go decrementing();
+
 	
     // We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
     // We will do it properly with channels soon. For now: Sleep.
