7707971074de553d1f221606664591ccfc97ff86
Lima98 <janoivil@stud.ntnu.no>
2024-02-07 12:01:00 +0100

    Exercise 4 approved :)

--- Exercises/Exercise 4/B.go
+++ Exercises/Exercise 4/B.go
@@ -9,14 +9,40 @@ import (
 	"time"
 )
 
-const proto, addr = "udp", ":20022"
+const proto, addr = "udp", "localhost:20022"
 const dataFile = "./data.txt"
 var programtype = 1 //0 is primary, 1 is backup
 
+
+func main() {
+	data, _ := os.ReadFile(dataFile)
+	counter, _ := strconv.Atoi(string(data))
+
+	for {
+		switch programtype {
+		case 0:
+			counter = readFromFile()
+			send()
+			time.Sleep(1 * time.Second)
+			counter++
+			fmt.Println("I AM THE MASTER NOW")
+			fmt.Println(counter)
+			writeToFile(strconv.FormatInt(int64(counter),10))
+		case 1:
+			receive()
+		}
+	}
+}
+
 func send(){
-	addr2 := "localhost:20022"
-	conn, _ := net.Dial(proto,addr2)                                                                                  
-	conn.Write([]byte("I'm alive!"))                                                                               
+	conn, _ := net.Dial(proto,addr)                                                                                  
+	conn.Write([]byte("I am alive"))                                                                               
+}
+
+func readFromFile() int {
+	data, _ := os.ReadFile(dataFile)
+	counter, _ := strconv.Atoi(string(data))
+	return counter
 }
 
 func writeToFile(data string){
@@ -26,41 +52,26 @@ func writeToFile(data string){
 func receive() {
 	conn, _ := net.ListenPacket(proto, addr)
 	
-	//for {
 		conn.SetReadDeadline(time.Now().Add(2 * time.Second))
 		buf := make([]byte, 1024)
 		num_of_bytes, source, err := conn.ReadFrom(buf)
 		//HER HAR VI IF, IKKE DREP OSS PLZ <3
 		if err != nil {
-			fmt.Println("NO DATA RECEIVED FOR 2 SECONDS")
+			conn.Close()
+			fmt.Println("NO MASTER FOUND")
 			programtype = 0
+			fmt.Println("Going master and making backup")
 			createBackup()
 		}else {
 			fmt.Println(string(buf[:num_of_bytes]))
 			conn.WriteTo(buf,source)
+			fmt.Println("-received from master")
 			programtype = 1
+			conn.Close()
 		}
 
-	//}
 }
 
 func createBackup() {
 	exec.Command("gnome-terminal", "--", "go", "run", "B.go").Run()
-}
-
-func main() {
-	//data, _ := os.ReadFile(dataFile)
-	//counter, _ := strconv.Atoi(string(data))
-	var counter = 0
-	for {
-		switch programtype {
-		case 0:
-			writeToFile(strconv.FormatInt(int64(counter),10))
-			send()
-			time.Sleep(1 * time.Second)
-			counter++
-		case 1:
-			receive()
-		}
-	}
 }
