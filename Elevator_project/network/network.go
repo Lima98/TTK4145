172c1e5e7bd5ce49a836ed1ac609a3ca9f421b99
Vegard Heggland <vegarmhe@stud.ntnu.no>
2024-02-27 15:42:52 +0100

    downloaded network module

--- Elevator_project/network/network.go
+++
@@ -1,9 +0,0 @@
-package network
-
-import (
-	"fmt"
-	"time"
-)
-
-
-
--- Exercises/Exercise 4/B.go
+++ Exercises/Exercise 4/B.go
@@ -11,8 +11,8 @@ import (
 
 const proto, addr = "udp", "localhost:20022"
 const dataFile = "./data.txt"
-var programtype = 1 //0 is primary, 1 is backup
 
+var programtype = 1 //0 is primary, 1 is backup
 
 func main() {
 	data, _ := os.ReadFile(dataFile)
@@ -27,16 +27,16 @@ func main() {
 			counter++
 			fmt.Println("I AM THE MASTER NOW")
 			fmt.Println(counter)
-			writeToFile(strconv.FormatInt(int64(counter),10))
+			writeToFile(strconv.FormatInt(int64(counter), 10))
 		case 1:
 			receive()
 		}
 	}
 }
 
-func send(){
-	conn, _ := net.Dial(proto,addr)                                                                                  
-	conn.Write([]byte("I am alive"))                                                                               
+func send() {
+	conn, _ := net.Dial(proto, addr)
+	conn.Write([]byte("I am alive"))
 }
 
 func readFromFile() int {
@@ -45,30 +45,30 @@ func readFromFile() int {
 	return counter
 }
 
-func writeToFile(data string){
+func writeToFile(data string) {
 	os.WriteFile("./data.txt", []byte(data), 0644)
 }
 
 func receive() {
 	conn, _ := net.ListenPacket(proto, addr)
-	
-		conn.SetReadDeadline(time.Now().Add(2 * time.Second))
-		buf := make([]byte, 1024)
-		num_of_bytes, source, err := conn.ReadFrom(buf)
-		//HER HAR VI IF, IKKE DREP OSS PLZ <3
-		if err != nil {
-			conn.Close()
-			fmt.Println("NO MASTER FOUND")
-			programtype = 0
-			fmt.Println("Going master and making backup")
-			createBackup()
-		}else {
-			fmt.Println(string(buf[:num_of_bytes]))
-			conn.WriteTo(buf,source)
-			fmt.Println("-received from master")
-			programtype = 1
-			conn.Close()
-		}
+
+	conn.SetReadDeadline(time.Now().Add(2 * time.Second))
+	buf := make([]byte, 1024)
+	num_of_bytes, source, err := conn.ReadFrom(buf)
+	//HER HAR VI IF, IKKE DREP OSS PLZ <3
+	if err != nil {
+		conn.Close()
+		fmt.Println("NO MASTER FOUND")
+		programtype = 0
+		fmt.Println("Going master and making backup")
+		createBackup()
+	} else {
+		fmt.Println(string(buf[:num_of_bytes]))
+		conn.WriteTo(buf, source)
+		fmt.Println("-received from master")
+		programtype = 1
+		conn.Close()
+	}
 
 }
 
